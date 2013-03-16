4812716b466036194cedfad2c5a2208328a04088
Lorenzo Lotto <llorenzo@outlook.com>
2013-03-16 19:55:13 +0100

    added things
    added startpage for email
    added multilang support
    boh, mamba

---
+++ aMUX/LangsManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace aMUX
+{
+  public class LangsManager
+  {
+    public Languages.LangsRes LangsRes { get {return _langRes;} }
+
+    private static Languages.LangsRes _langRes = new Languages.LangsRes();
+
+    public LangsManager()
+    {
+
+    }
+  }
+}
---
+++ aMUX/StartPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace aMUX
+{
+  public partial class StartPage : PhoneApplicationPage
+  {
+    public StartPage()
+    {
+      InitializeComponent();
+    }
+
+    private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+      if (txtBxEmail.Text.Contains('@'))
+      {
+        btnConfMail.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 0, 100, 0));
+        btnConfMail.Content = Languages.LangsRes.btnConfMailOK;
+      }
+      else
+      {
+        btnConfMail.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+        btnConfMail.Content = Languages.LangsRes.btnConfMailFAIL;
+      }
+
+    }
+
+    private void btnConfMail_Click(object sender, RoutedEventArgs e)
+    {
+      NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+    }
+  }
+}
