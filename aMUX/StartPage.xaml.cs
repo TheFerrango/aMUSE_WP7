f145fd0e4f85b50b4eaf3a028b9e438169ba163c
Lorenzo Lotto <llorenzo@outlook.com>
2013-03-17 15:12:32 +0100

    qr support
    added new library to implement qr code decoding capability

--- aMUX/StartPage.xaml.cs
+++ aMUX/StartPage.xaml.cs
@@ -10,34 +10,51 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using System.Text.RegularExpressions;
+using System.IO.IsolatedStorage;
 
 namespace aMUX
 {
   public partial class StartPage : PhoneApplicationPage
   {
+    bool isValid;
+
     public StartPage()
     {
       InitializeComponent();
+      isValid = false;
     }
 
     private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
     {
-      if (txtBxEmail.Text.Contains('@'))
+      Regex r = new Regex(@"^(([^<>()[\]\\.,;:\s@\""]+"
+                        + @"(\.[^<>()[\]\\.,;:\s@\""]+)*)|(\"".+\""))@"
+                        + @"((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+                        + @"\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+"
+                        + @"[a-zA-Z]{2,}))$");
+      if (r.IsMatch(txtBxEmail.Text))
       {
         btnConfMail.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 0, 100, 0));
         btnConfMail.Content = Languages.LangsRes.btnConfMailOK;
+        isValid = true;
       }
       else
       {
         btnConfMail.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
         btnConfMail.Content = Languages.LangsRes.btnConfMailFAIL;
+        isValid = false;
       }
 
     }
 
     private void btnConfMail_Click(object sender, RoutedEventArgs e)
     {
-      NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+      IsolatedStorageSettings iss = IsolatedStorageSettings.ApplicationSettings;
+      if (isValid)
+      {
+        NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        iss["emailAddress"] = txtBxEmail.Text;
+      }
     }
   }
 }
